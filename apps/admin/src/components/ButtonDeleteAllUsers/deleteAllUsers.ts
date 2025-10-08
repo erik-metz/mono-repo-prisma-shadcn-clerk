7f6e4a5c7b656ec493b82ff51ddc3f64f2530e8d
erik-metz <erik.metz.info@gmail.com>
2025-10-08 15:42:23 +0200

    adding logs

--- apps/admin/src/components/ButtonDeleteAllUsers/deleteAllUsers.ts
+++ apps/admin/src/components/ButtonDeleteAllUsers/deleteAllUsers.ts
@@ -1,9 +1,11 @@
 'use server'
 
 import { prisma } from '@repo/database'
+import { logger } from '@repo/utils/logger'
 import { revalidatePath } from 'next/cache'
 
 export const deleteAllUsers = async () => {
+  logger.log('in deleteAllUsers')
   await prisma.user.deleteMany()
   revalidatePath('/')
 }
