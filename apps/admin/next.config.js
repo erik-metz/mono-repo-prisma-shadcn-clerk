0f323381a463d5f8ccce76f7842e910bd750ae35
erik-metz <erik.metz.info@gmail.com>
2025-10-08 16:41:13 +0200

    fix

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,6 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 module.exports = {
-  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 }
