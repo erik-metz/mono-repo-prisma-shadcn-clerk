7cbb156b5389db9f6d2ff6bb467b252990ead321
erik-metz <erik.metz.info@gmail.com>
2025-10-08 12:28:00 +0200

    back

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,11 +6,7 @@ export default async function IndexPage() {
   return (
     <div>
       <h1>Hello World</h1>
-      <div>
-        {users.map((user: { id: string; name: string }) => (
-          <p key={user.id}>{user.name}</p>
-        ))}
-      </div>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   );
 }
