e144b44600add431f7a4eceabca45b5850f2c55d
erik-metz <erik.metz.info@gmail.com>
2025-10-08 14:21:56 +0200

    adding admin

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,20 @@
+import { prisma } from '@repo/database'
+import { Button } from '@repo/ui/components/button'
+
+export default async function IndexPage() {
+  const users = await prisma.user.findMany()
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-3xl font-semibold">Hello Admin</h1>
+      <Button className="w-30">Hi!</Button>
+      <div>
+        {users.map((user, index) => (
+          <p key={user.id}>
+            {index + 1}. {user.email}
+          </p>
+        ))}
+      </div>
+    </div>
+  )
+}
