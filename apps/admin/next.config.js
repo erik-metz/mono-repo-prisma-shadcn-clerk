dcc1c00800057a1394fade3144e3c3cf37686b9d
erik-metz <erik.metz.info@gmail.com>
2025-10-08 16:27:09 +0200

    fix

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@repo/database'],
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+  reactStrictMode: true,
 }
-
-module.exports = nextConfig
