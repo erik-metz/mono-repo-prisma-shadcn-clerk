795f6adc1fd13026d8feb64ad72131f1598c82ea
erik-metz <erik.metz.info@gmail.com>
2025-10-08 13:43:22 +0200

    adding shadcn

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,15 @@
 import '@repo/ui/globals.css'
+import { Geist, Geist_Mono } from 'next/font/google'
+
+const fontSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+const fontMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+})
 
 export default function RootLayout({
   children,
@@ -7,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,20 @@
 import { prisma } from '@repo/database'
+import { Button } from '@repo/ui/components/button'
 
 export default async function IndexPage() {
   const users = await prisma.user.findMany()
 
   return (
-    <div>
-      <h1 className="border-5 border-green-500 text-3xl font-semibold">
-        Hello World
-      </h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+    <div className="flex flex-col gap-2">
+      <h1 className="text-3xl font-semibold">Hello World</h1>
+      <Button className="w-30">Hi!</Button>
+      <div>
+        {users.map((user, index) => (
+          <p key={user.id}>
+            {index + 1}. {user.email}
+          </p>
+        ))}
+      </div>
     </div>
   )
 }
